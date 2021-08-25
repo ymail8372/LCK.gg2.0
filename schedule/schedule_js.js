f1bdccfd779a22455831d50f83815030d4847668
ymail <ymail8372.naver.com>
2021-08-25 23:41:25 +0900

    making_210825

--- schedule/schedule_js.js
+++ schedule/schedule_js.js
@@ -1,6 +1,8 @@
-var prevMonth = "1";
+var prevMonth = "none";
+var nowMonth = "month9";
 
 function monthClick(value) {
+	nowMonth = 'month' + value;
 	var idName = "month" + value;
 	var idMenuName = "month_menu" + value;
 	var delIdName = "";
@@ -9,16 +11,38 @@ function monthClick(value) {
 		if (value != i) {
 			delIdName = "month" + i;
 			document.getElementById(delIdName).style.display = 'none';
-			document.getElementById(idMenuName).style.borderBottom = '2px solid #1C192B';
-			if (prevMonth.startsWith('m')) {
-				document.getElementById(prevMonth).style.removeProperty('border-bottom');
-			}
-		}
-		else {
-			continue;
 		}
 	}
+
+	if (prevMonth != "none") {
+		document.getElementById(prevMonth).style.removeProperty('border-bottom');
+	}
+
 	document.getElementById(idMenuName).style.borderBottom = '2px solid #1C192B';
 	document.getElementById(idName).style.display = 'block';
 	prevMonth = "month_menu" + value;
 };
+
+var prevWeek = "none";
+
+function weekClick(value) {
+	var className = "week" + value;
+	var idMenuName = "week_menu" + value;
+	var delClassName = "";
+
+	for(let i = 1; i <= 4; i ++) {
+		if (value != i) {
+			delClassName = "week" + i;
+			document.querySelector('#' + nowMonth + ' .' + className).style.display = 'none';
+			// document.getElementsByClassName(delClassName).style.display = 'none';
+		}
+	}
+
+	if (prevWeek != "none") {
+		document.getElementById(prevWeek).style.removeProperty('border-bottom');
+	}
+
+	document.getElementById(idMenuName).style.borderBottom = '2px solid #1C192B';
+	document.querySelector('#' + nowMonth + ' .' + className).style.display = 'block';
+	prevWeek = "week_menu" + value;
+}
