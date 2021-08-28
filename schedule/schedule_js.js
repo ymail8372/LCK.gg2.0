938798e3d0a10d17fdcd48d2a2b791f017b84611
ymail <ymail8372.naver.com>
2021-08-28 23:33:39 +0900

    making_210828

--- schedule/schedule_js.js
+++ schedule/schedule_js.js
@@ -1,4 +1,4 @@
-var prevMonth = "none";
+var prevMonth = "month_menu9";
 var nowMonth = "month9";
 
 function monthClick(value) {
@@ -7,42 +7,58 @@ function monthClick(value) {
 	var idMenuName = "month_menu" + value;
 	var delIdName = "";
 
+	// 모든 month display 없앰
 	for(let i = 1; i <= 12; i ++) {
 		if (value != i) {
 			delIdName = "month" + i;
 			document.getElementById(delIdName).style.display = 'none';
 		}
 	}
-
-	if (prevMonth != "none") {
-		document.getElementById(prevMonth).style.removeProperty('border-bottom');
-	}
-
+	// 선택한 month 나타남
 	document.getElementById(idMenuName).style.borderBottom = '2px solid #1C192B';
 	document.getElementById(idName).style.display = 'block';
+	weekClick('Total')
+
+	// border-bottom 없앰
+	document.getElementById(prevMonth).style.removeProperty('border-bottom');
+
+	// prevMonth 갱신
 	prevMonth = "month_menu" + value;
 };
 
-var prevWeek = "none";
+var prevWeek = "week_menuTotal";
 
 function weekClick(value) {
 	var className = "week" + value;
 	var idMenuName = "week_menu" + value;
 	var delClassName = "";
-
-	for(let i = 1; i <= 4; i ++) {
-		if (value != i) {
-			delClassName = "week" + i;
-			document.querySelector('#' + nowMonth + ' .' + className).style.display = 'none';
-			// document.getElementsByClassName(delClassName).style.display = 'none';
+	
+	// 모든 주차 선택할 경우
+	if (value == 'Total') {
+		for(let i = 1; i <= 4; i ++) {
+			if (value != i) {
+				ClassName = "week" + i;
+				document.querySelector('#' + nowMonth + ' .' + ClassName).style.display = 'block';
+			}
 		}
 	}
-
-	if (prevWeek != "none") {
-		document.getElementById(prevWeek).style.removeProperty('border-bottom');
+	// 특정 주차 선택할 경우
+	else{
+		for(let i = 1; i <= 4; i ++) {
+			if (value != i) {
+				delClassName = "week" + i;
+				document.querySelector('#' + nowMonth + ' .' + delClassName).style.display = 'none';
+			}
+		}
+		document.querySelector('#' + nowMonth + ' .' + className).style.display = 'block';
 	}
 
+	// border-bottom 없앰
+	document.getElementById(prevWeek).style.removeProperty('border-bottom');
+
+	// border-bottom 생성
 	document.getElementById(idMenuName).style.borderBottom = '2px solid #1C192B';
-	document.querySelector('#' + nowMonth + ' .' + className).style.display = 'block';
+
+	// prevWeek 업데이트
 	prevWeek = "week_menu" + value;
 }
